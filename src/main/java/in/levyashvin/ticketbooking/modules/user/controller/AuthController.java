9128870989820f78d2baefc9e72aaf7984636de4
guava <yuvayashvin@gmail.com>
2025-12-24 00:03:10 +0530

    cleaned files

--- src/main/java/in/levyashvin/ticketbooking/modules/user/controller/AuthController.java
+++ src/main/java/in/levyashvin/ticketbooking/modules/user/controller/AuthController.java
@@ -1,10 +1,8 @@
 package in.levyashvin.ticketbooking.modules.user.controller;
 
-import org.aspectj.lang.annotation.RequiredTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.levyashvin.ticketbooking.modules.user.dto.AuthenticationRequest;
