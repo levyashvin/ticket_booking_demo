aa67cbb6bffd6707b1f21a8c692d1f1e9a19cb6a
guava <yuvayashvin@gmail.com>
2025-12-29 21:39:01 +0530

    added rabbitmq config file

---
+++ src/main/java/in/levyashvin/ticketbooking/config/RabbitMQConfig.java
@@ -0,0 +1,46 @@
+package in.levyashvin.ticketbooking.config;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_NAME = "email_queue";
+    public static final String EXCHANGE_NAME = "booking_exchange";
+    public static final String ROUTING_KEY = "email_routing_key";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, true);
+    }
+    
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public JacksonJsonMessageConverter converter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+    public AmqpTemplate ampqTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
+    }
+}
